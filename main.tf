1cd86598f9292fa4a1ab8feffc83019f079b9506
Ivan Dvylyuk <ivan.dvylyuk@gmail.com>
2024-03-18 13:32:08 +0200

    fixed vnet configuration

--- main.tf
+++ main.tf
@@ -28,5 +28,5 @@ resource "azurerm_virtual_network" "vnet" {
     name                = "myTFVNet"
     resource_group_name = azurerm_resource_group.rg.name
     location            = azurerm_resource_group.rg.location
-    address_space = ["10.0.0.1/16"]
+    address_space = ["10.0.0.0/16"]
 }
